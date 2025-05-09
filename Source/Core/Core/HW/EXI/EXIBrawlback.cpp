248005e5564b74077e8d9410e69049f0cc55b585
SilverMight <joeyzee1234@gmail.com>
2025-05-09 14:21:05 -0400

    EXIBrawlback: fix incorrect fmt string

--- Source/Core/Core/HW/EXI/EXIBrawlback.cpp
+++ Source/Core/Core/HW/EXI/EXIBrawlback.cpp
@@ -1212,7 +1212,7 @@ void CEXIBrawlback::handleEndMatch(u8* payload)
 
   if (res != 0)
   {
-    ERROR_LOG_FMT(BRAWLBACK, "[GameReport] Got error executing request. Err code: {#d}", (int)res);
+    ERROR_LOG_FMT(BRAWLBACK, "[GameReport] Got error executing request. Err code: {:d}", (int)res);
   }
   else
   {
